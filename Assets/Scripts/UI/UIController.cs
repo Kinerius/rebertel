5fa4b7e8ae77ce0e0a6fc138f36b6992baf72847
Patricio Guerra <kinerius@gmail.com>
2021-01-30 19:44:22 -0300

    Updated level flow

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,4 +1,6 @@
+using System;
 using Character;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,19 +8,42 @@ namespace UI
 {
     public class UIController : MonoBehaviour
     {
+        private static UIController instance;
+        public static UIController Instance => instance;
+        
         [SerializeField] private Image healthBarFill;
+        [SerializeField] private RectTransform countdownContainer;
+        [SerializeField] private TextMeshProUGUI countdownText;
         private EntityController _player;
+
+        private void Awake()
+        {
+            instance = this;
+        }
 
         void Start()
         {
             _player = GameObject.Find("Player").GetComponent<EntityController>();
             _player.OnHealthChangedEvent += OnHealthChanged;
+
             healthBarFill.fillAmount = 1;
         }
+
 
         private void OnHealthChanged(int maxHealth, int minHealth)
         {
             healthBarFill.fillAmount = minHealth / (float)maxHealth;
+        }
+
+        public void SetCountdown(long countdown)
+        {
+            if (countdown < 0)
+            {
+                countdownContainer.gameObject.SetActive(false);
+                return;
+            }
+            countdownContainer.gameObject.SetActive(true);
+            countdownText.text = countdown.ToString();
         }
     }
 }
