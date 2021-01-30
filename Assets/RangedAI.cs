917274c81433b8abdb6ef1153c4fdd0c2fe300d4
Agustin T <agustintorchia@gmail.com>
2021-01-30 17:58:51 -0300

    Movimiento muy basico agregado

--- Assets/RangedAI.cs
+++ Assets/RangedAI.cs
@@ -11,6 +11,7 @@ namespace Character
 
         private GameObject player;
 
+        private float _movingTimer;
         private void Start()
         {
             player = GameObject.Find("Player");
@@ -18,10 +19,27 @@ namespace Character
         private void Update()
         {
             ShootAtPlayer();
+            MoveAround();
+            UpdateCooldowns();
         }
         private void ShootAtPlayer()
         {
             entityTarget.ShootAt(player.transform.position);
+            
         }
+
+        private void MoveAround()
+        {
+            if (_movingTimer > 0) return;
+            var randomPoint = new Vector3(Random.Range(-2,2),0, Random.Range(-2,2));
+            Debug.Log(randomPoint);
+            entityTarget.SetMovementDirection(randomPoint);
+            _movingTimer = 1 / 1;
+        }
+        private void UpdateCooldowns()
+        {
+            _movingTimer = Mathf.Max(_movingTimer - Time.deltaTime, 0);
+        }
+
     }
 }
